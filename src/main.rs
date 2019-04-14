ceda0f5ef60ab698164b86430850ac73f0eeca38
Jie Yang <jie.yang@nanchao.org>
2019-04-14 15:45:39 +0800

    First workable version

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+// This example demonstrates use of the `FutureResolver`.
+extern crate c_ares;
+extern crate c_ares_resolver;
+extern crate futures;
+extern crate tokio;
+
+use std::error::Error;
+
+use c_ares_resolver::{CAresFuture, FutureResolver, Options};
+use futures::future::{join_all, Future};
+use futures::stream::FuturesUnordered;
+use futures::{future, Stream};
+use std::thread;
+use std::time;
+use std::net::Ipv4Addr;
+
+fn genRutureResolve(server: &str) -> CAresFuture<c_ares::AResults> {
+    let mut option = Options::new();
+    option.set_timeout(2000);
+    let resolver = FutureResolver::with_options(option).expect("Failed to create resolver");
+    resolver.set_servers(&[server]).expect("Fail to set server");
+    resolver.query_a("baidu.com")
+}
+fn main() {
+    // Create resolver and make a query.
+    let servers = ["8.8.8.8:53", "192.168.1.1:53"];
+
+    let mut future_set = FuturesUnordered::<CAresFuture<c_ares::AResults>>::new();
+
+    servers
+        .into_iter()
+        .for_each(|server| future_set.push(genRutureResolve(server)));
+
+    let future = future_set
+        .map_err(|e| {
+            println!("dns lookup failed with error '{}'", e.description());
+            ()
+        })
+        .collect();
+
+    let task = future.map(|items| {
+        let result: Vec<c_ares::AResult> = items.iter().flat_map(|item| item.into_iter()).collect();
+
+        let mut to_show = result.into_iter().map(|results| {
+            results.ipv4()
+        }).collect::<Vec<Ipv4Addr>>();
+
+        to_show.sort();
+        to_show.dedup();
+        println!("to show is {:?}", to_show);
+        ()
+    });
+    tokio::run(task);
+}
